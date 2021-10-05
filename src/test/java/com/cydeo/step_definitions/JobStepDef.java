6f1b9fbf5c21147fd0bb9d3c1f902327cd83b4fd
Altynai23 <salyb1987@gmail.com>
2021-10-05 11:07:20 -0400

    Cucumber Day 02 : jobs feature step definitions

---
+++ src/test/java/com/cydeo/step_definitions/JobStepDef.java
@@ -0,0 +1,38 @@
+package com.cydeo.step_definitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class JobStepDef {
+
+    @Given("student attend all the classes")
+    public void student_attend_all_the_classes() {
+
+    }
+    @When("student learn all the topics")
+    public void student_learn_all_the_topics() {
+
+    }
+    @Then("student should be ready for the job")
+    public void student_should_be_ready_for_the_job() {
+
+    }
+    @Given("student is prepared")
+    public void student_is_prepared() {
+        System.out.println("Given student is prepared");
+
+    }
+    @When("student upload their resume")
+    public void student_upload_their_resume() {
+        System.out.println("When student upload their resume");
+
+    }
+    @Then("student will be in the market")
+    public void student_will_be_in_the_market() {
+        System.out.println("Then student will be in the market");
+
+    }
+
+
+}
